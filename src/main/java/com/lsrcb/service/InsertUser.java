f01442bafcfc5ca41e55c1c5b96fccc6525749d7
jefflsnsh <jeff.lsnsh@gmail.com>
2017-05-21 01:28:56 +0800

    新增前端主界面

--- src/main/java/com/lsrcb/service/InsertUser.java
+++ src/main/java/com/lsrcb/service/InsertUser.java
@@ -3,10 +3,17 @@ package com.lsrcb.service;
 import com.lsrcb.dao.UserInfoDao;
 import com.lsrcb.pojo.UserInfo;
 import org.apache.catalina.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.concurrent.Executors;
 
 /**
  * Created by 颜锐 on 2017/5/11
@@ -25,10 +32,25 @@ public class InsertUser {
         UserInfo userInfo = new UserInfo();
         userInfo.setId(1);
         userInfo.setUsername("YanRui");
-        userInfo.setPassword("123456");
+        userInfo.setPassword("34567");
         userInfoDao.insertOne(userInfo);
         System.out.println("用户名"+userInfoDao.getOne(9).getPassword());
+        Logger logger = LoggerFactory.getLogger(this.getClass());
+        logger.info("测试系统日志");
+
     }
 
+
+}
+@Configuration
+class MyWebConfiguratipon extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/js/");
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/css/");
+    }
 
 }
